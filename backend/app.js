08a7e429b4a91f129b8c1392e1eacd1c7a779949
“Anshika <ansgupta.01@gmail.com>
2023-04-12 20:26:13 +0530

    user & admin

--- backend/app.js
+++ backend/app.js
@@ -4,11 +4,14 @@ const app = express();
 const moongoose = require('mongoose');
 const dotenv = require('dotenv');
 const userRouter = require('./routes/user-routes');
+const adminRouter = require('./routes/admin-routes');
 dotenv.config();
 
 //middleware
 app.use(express.json());
 app.use("/user", userRouter);
+app.use("/admin", adminRouter);
+
 
 moongoose.connect("mongodb+srv://Anshika66-Gupta:6MSWTVddULDUEy05@movie-system.87yhtky.mongodb.net/Movies?retryWrites=true&w=majority")
 
