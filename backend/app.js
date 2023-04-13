e1be8134be05cb7551c622155fd0aeba7369453e
“Anshika <ansgupta.01@gmail.com>
2023-04-13 20:12:58 +0530

    admin done, movie route & controller token

--- backend/app.js
+++ backend/app.js
@@ -5,12 +5,15 @@ const moongoose = require('mongoose');
 const dotenv = require('dotenv');
 const userRouter = require('./routes/user-routes');
 const adminRouter = require('./routes/admin-routes');
+const movieRouter = require('./routes/movie-routes');
 dotenv.config();
 
 //middleware
 app.use(express.json());
 app.use("/user", userRouter);
 app.use("/admin", adminRouter);
+app.use("/movie", movieRouter);
+
 
 
 moongoose.connect("mongodb+srv://Anshika66-Gupta:6MSWTVddULDUEy05@movie-system.87yhtky.mongodb.net/Movies?retryWrites=true&w=majority")
