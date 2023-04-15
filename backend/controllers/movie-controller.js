cda21907afa989ff74765152ed0971783e41caea
“Anshika <ansgupta.01@gmail.com>
2023-04-15 20:56:50 +0530

    movie & booking

--- backend/controllers/movie-controller.js
+++ backend/controllers/movie-controller.js
@@ -59,8 +59,40 @@ const addMovie = async (req, res, next) => {
         return res.status(500).json({ message: "Request Failed" })
     }
 
-    return res.status(201).json({ movie})
-    
+    return res.status(201).json({ movie})  
 };
+const getAllMovie = async (req, res, next) => {
+    
+    let movies;
+    try {
+        movies = await Movie.find();
+    } catch (err) {
+        return console.log(err);
+    }
+    
+    if (!movies) {
+        {
+            return res.status(500)({ message: "Request Failed" })
+        }
+    }
+    return res.status(200).json({ movies })
 
-module.exports = { addMovie }       
+}
+const getMovieById = async (req, res, next) => { 
+    const id = req.params.id;
+    let movie;
+    try {
+        movie = await Movie.findById(id);
+        
+    }
+    catch (err) { 
+        return console.log(err);
+    }
+    if (!movie) {
+        return res.status(404).json({ message: "Invalid Movie ID" });
+    }
+    return res.status(200).json ({movie})
+}
+
+
+module.exports = { addMovie, getAllMovie, getMovieById }  
---
+++ backend/routes/booking-routes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const bookingsRouter = express.Router();
+
+bookingsRouter.post('/', );
+
+
+module.exports= bookingsRouter;
