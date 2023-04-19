78c0cecc4dc61de236e229ab8094e5190bf90562
“Anshika <ansgupta.01@gmail.com>
2023-04-19 20:00:17 +0530

    movies

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,15 @@ const movieRouter = require('./routes/movie-routes');
 const bookingRouter = require('./routes/booking-routes');
 dotenv.config();
 
+const cors = require('cors');
+app.use(cors());
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET', 'POST', 'PUT', 'DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization'); 
+    next();
+    
+})
 //middleware
 app.use(express.json());
 app.use("/user", userRouter);
